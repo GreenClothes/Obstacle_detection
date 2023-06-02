8dd90a817f2453bbf230fec4d9f92f8481bd6522
GreenClothes <kybin.kim@gmail.com>
2023-06-02 21:02:38 +0900

    perspective transforming for VAE

---
+++ AWS_detection/lib/perspective_trans.py
@@ -0,0 +1,21 @@
+import cv2, numpy as np
+from param import _img_w, _img_h, _source_img_dir
+
+def _Ptrans(road):
+    img = cv2.imread(_source_img_dir, cv2.IMREAD_COLOR)
+    trans_img = []
+
+    for r in road:
+        topLeft = r[0]
+        topRight = r[1]
+        bottomRight = r[2]
+        bottomLeft = r[3]
+
+        before_trans = np.float32([topLeft, topRight, bottomRight, bottomLeft])
+        after_trans = np.float32([[0, 0], [128, 0], [128, 128], [0, 128]])
+
+        trans_mtrx = cv2.getPerspectiveTransform(before_trans, after_trans)
+
+        trans_img.append(cv2.warpPerspective(img, trans_mtrx, (128, 128)))
+
+    return trans_img
