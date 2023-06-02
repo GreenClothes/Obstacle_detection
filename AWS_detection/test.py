f70dd71a579349d820d8edae9f4735e565070839
GreenClothes <kybin.kim@gmail.com>
2023-06-02 21:03:48 +0900

    just test file

--- AWS_detection/test.py
+++ AWS_detection/test.py
@@ -1,15 +1,9 @@
 import numpy as np
 
-a = [1, 2, 3]
-b = [4, 5, 6]
+a = [[[7, 1], [5, 5], [1, 4], [5, 6]],
+     [[4, 5], [4, 4], [1, 7], [6, 3]],
+     [[5, 6], [8, 8], [1, 2], [4, 5]]]
+b = [0, 2, 4]
 
-def ab(a, b):
-    m = int(input().strip())
-    if m < 5:
-        a[0] = b[0]
-    else:
-        a[0] = b[1]
-
-while 1:
-    ab(a, b)
-    print(a)
+a = sorted(a, key=lambda a:a[1][1])
+print(a)
