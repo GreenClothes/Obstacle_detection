b2f2d87d8ec1655f88379592d9fcb913187e5219
GreenClothes <kybin.kim@gmail.com>
2023-06-02 20:59:27 +0900

    redesignate the ROI

--- AWS_detection/Lane-detection-with-OpenCV/ROI_test.py
+++ AWS_detection/Lane-detection-with-OpenCV/ROI_test.py
@@ -5,10 +5,12 @@ img = cv2.imread('images/traffic.png')
 def ROI(img):
     img_h = img.shape[0]
     img_w = img.shape[1]
-
+    '''
     region = np.array([[0, img_h * 0.76], [img_w * 0.31, img_h * 0.58], [img_w * 0.79, img_h * 0.58],
                        [img_w, img_h * 0.72], [img_w, img_h*0.9], [0, img_h*0.9]], np.int32)
-
+    '''
+    region = np.array([[img_w * 0.31, img_h * 0.6], [img_w * 0.79, img_h * 0.6],
+                       [img_w, img_h * 0.9], [0, img_h * 0.9]], np.int32)
     mask = np.zeros((img_h, img_w, 3), np.uint8)
     cv2.fillPoly(mask, [region], (255, 255, 255))
     #cv2.imshow('mask', mask)
