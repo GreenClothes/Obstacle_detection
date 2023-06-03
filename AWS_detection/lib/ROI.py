8c1a2293966e525eab933ff1eff5fec1561dd409
GreenClothes <kybin.kim@gmail.com>
2023-06-03 21:27:27 +0900

    modifying VAE weights dir

--- AWS_detection/lib/ROI.py
+++ AWS_detection/lib/ROI.py
@@ -1,5 +1,5 @@
 import cv2, numpy as np
-from param import _img_w, _img_h, _lane_classes
+from lib.param import _img_w, _img_h, _lane_classes
 
 def _XY2ROI(car, roadmark):
 
@@ -17,7 +17,7 @@ def _XY2ROI(car, roadmark):
     lane_bbox = sorted(lane_bbox, key=lambda lane_bbox:lane_bbox[1][0])
     ROI = [[lane_bbox[0][1][0], lane_bbox[0][1][1]], [lane_bbox[-1][0][0], lane_bbox[-1][0][1]],
            [lane_bbox[-1][2][0], lane_bbox[-1][2][1]], [lane_bbox[0][3][0], lane_bbox[0][3][1]]]
-    ROI = [[int(ROI[i][0] * _img_w), int(ROI[i][1] * _img_h)] for i in range(len(ROI))]
+    #ROI = [[int(ROI[i][0] * _img_w), int(ROI[i][1] * _img_h)] for i in range(len(ROI))]
 
     # Get road lane area
     road_area = _get_road_lane(ROI, lane_bbox)
@@ -59,25 +59,30 @@ def _XY2ROI(car, roadmark):
 def _get_road_lane(roi, lane):
     lane_coeff = []
     road_area = []
-    road_middle = (max(roi[0]) - min(roi[0])) // 2
-
+    road_middle = (roi[2][0] + roi[3][0]) // 2
     for i in range(len(lane)):
         # At first, checking lanes that are in the ROI.
         # ROI top pixel < lane bottom pixel AND ROI bottom pixel > lane top pixel
-        if roi[0][1] <= lane[i][2][1] and roi[2][1] >= lane[i][0][1]:
+        if roi[0][1] <= lane[i][2][1] or roi[2][1] >= lane[i][0][1]:
             # Checking lane direction
-            lane_middle = (lane[i][0][0] - lane[i][1][0]) // 2
+            lane_middle = (lane[i][1][0] + lane[i][0][0]) // 2
             # Calculating coefficient of lane linear function
             if road_middle > lane_middle:
                 a = np.linalg.inv([[lane[i][1][0], 1], [lane[i][3][0], 1]])
                 b = [[lane[i][1][1]], [lane[i][3][1]]]
                 coeff = np.dot(a, b)
-                lane_coeff.append([int(coeff[0][0]), int(coeff[1][0])])
+                lane_coeff.append([coeff[0][0], coeff[1][0]])
             else:
                 a = np.linalg.inv([[lane[i][0][0], 1], [lane[i][2][0], 1]])
                 b = [[lane[i][0][1]], [lane[i][2][1]]]
                 coeff = np.dot(a, b)
-                lane_coeff.append([int(coeff[0][0]), int(coeff[1][0])])
+                lane_coeff.append([coeff[0][0], coeff[1][0]])
+
+    #########################################################
+    # opencv로 lane_coeff에 따라서 이미지 위에 직선 그려보기
+    # cv2.line(img, pt1, pt2, color[, thickness[, lineType[, shift]]]) → img
+    # cv2.line(black_canvas, (10, 100), (500, 100), green)
+    #########################################################
 
     # Divide ROI into road areas
     # Divide in order from the left
